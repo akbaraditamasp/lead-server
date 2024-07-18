e4deb8351cc52ace141495bb5af2e787e737f69c
akbaraditamasp <akbaraditamasp@gmail.com>
2024-07-18 17:44:58 +0700

    fix: load rxdb

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import rxdb from "./modules/rxdb.js";
 import server from "./modules/server.js";
 
-export default function createServer(sitesPath: string, port?: number) {
+export default async function createServer(sitesPath: string, port?: number) {
+  await rxdb.boot();
   server(sitesPath, port);
 }
